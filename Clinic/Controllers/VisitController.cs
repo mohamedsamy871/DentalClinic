22aae5429ac0eee9ad323cc195e556f33a2aff6c
mohamedsamy871 <mohamedsamy871@hotmail.com>
2022-04-26 02:32:07 +0200

    visit

---
+++ Clinic/Controllers/VisitController.cs
@@ -0,0 +1,52 @@
+﻿using Clinic.Interfaces;
+using Clinic.ViewModels;
+using DataAccess.Data;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinic.Controllers
+{
+    public class VisitController : Controller
+    {
+        private readonly IUnitOfWork<VisitInfo> _visit;
+        private readonly MyAppContext _db;
+        public VisitController(IUnitOfWork<VisitInfo> visit, MyAppContext db)
+        {
+            _visit = visit;
+            _db = db;
+        }
+        public IActionResult Index(int PatientId)
+        {
+            var patient = _db.Patients.Find(PatientId);
+            var patientVisits = _db.Visits.Where(m => m.PatientInfo.Id == PatientId).FirstOrDefault();
+            var Bill = _db.PatientBills.Where(m => m.Visit.Id == patientVisits.Id).FirstOrDefault();
+
+            var VisitDetails = new VisitViewModel() {
+                PatientInfo = patient,
+                VisitInfo =patientVisits,
+                PatientBill =Bill
+            };
+
+            return View(_visit.Entity.GetAll());
+        }
+
+        public IActionResult AddVisit(int PatientId)
+        {
+            ViewBag.PatientId = PatientId;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddVisit(VisitInfo visitInfo)
+        {
+            visitInfo.Date = DateTime.Now.ToString("d");
+            visitInfo.Time = DateTime.Now.ToString("t");
+            _visit.Entity.Add(visitInfo);
+            _visit.Save();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Clinic/ViewModels/VisitViewModel.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static DataAccess.Models.VisitInfo;
+
+namespace Clinic.ViewModels
+{
+    public class VisitViewModel
+    {
+        public ICollection<DoctorAssessment> DoctorAssessments { get; set; }
+        public PatientInfo PatientInfo { get; set; }
+        public PatientBill PatientBill { get; set; }
+        public VisitInfo VisitInfo { get; set; }
+        public VisitType Type { get; set; }
+        public List<SelectListItem> VisitTypes { get; set; }
+        public VisitViewModel()
+        {
+            VisitTypes = new List<SelectListItem>();
+            VisitTypes.Add(new SelectListItem { 
+                Value= ((int)VisitType.Consultation).ToString(),
+                Text = VisitType.Consultation.ToString()
+            });;
+            VisitTypes.Add(new SelectListItem
+            {
+                Value = ((int)VisitType.Examination).ToString(),
+                Text = VisitType.Examination.ToString()
+            }); ;
+        }
+    }
+}
