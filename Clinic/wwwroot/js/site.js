d22082e12a896782f4c6d0139a187ca1831df601
mohamedsamy871 <mohamedsamy871@gmail.com>
2022-04-28 11:54:07 +0200

    css

--- Clinic/wwwroot/js/site.js
+++ Clinic/wwwroot/js/site.js
@@ -1,4 +1,11 @@
 ﻿$(document).ready(function () {
+    $("body").niceScroll(
+        {
+            cursorcolor: "#40a9ff",
+            cursorwidth: "8px",
+            cursorborder:"1px solid transparent"
+        }
+    );
     var winheight = $(window).height();
     $('header').height(winheight);
     $('header .row').height(winheight);
